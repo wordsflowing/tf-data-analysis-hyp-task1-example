a7c0e8841d025d2e21dedfc73d4c464cef33de83
wordsflowing <129582590+wordsflowing@users.noreply.github.com>
2023-04-11 11:41:14 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
+from statsmodels.stats.proportion import proportions_ztest
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 422119389 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
@@ -11,4 +11,6 @@ def solution(x_success: int,
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    # Проводим z-тест на равенство конверсий
+    z_score, p_value = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], alternative='two-sided')
+    return p_value < 0.06
